8ce1c6c656a644f03d6051b7ea4475e182940247
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2022-04-23 12:28:49 +0530

    Update insta.cmd

--- instagram_profile_downloader/insta.cmd
+++ instagram_profile_downloader/insta.cmd
@@ -3,4 +3,5 @@
 set /p username="Enter the username: "
 echo username is: %username%
 
+@REM this command will download the enitre profile in the current directory with the username of the account.
 instaloader profile %username%
